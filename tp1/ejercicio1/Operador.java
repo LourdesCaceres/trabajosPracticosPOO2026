e2d9ce65e38f5d88d0993517bac548626427f42e
Lou <lourdesgabriela423@gmail.com>
2026-08-20 10:50:00 -0300

    Archivos del ej1

---
+++ tp1/ejercicio1/Operador.java
@@ -0,0 +1,15 @@
+
+public class Operador{
+    public static void main(String args []){
+        int a, b;
+        a = 8;
+        b = 3;
+        
+        System.out.println("Resultados de operar 8 y 3:");
+        System.out.println("Suma: "+(a+b));
+        System.out.println("Resta: "+(a-b));
+        System.out.println("Multiplicación: "+(a*b));
+        System.out.println("Division: "+(a/b));
+        System.out.println("Resto: "+(a%b));
+    }
+}
