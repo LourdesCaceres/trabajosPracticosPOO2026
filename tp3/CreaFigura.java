90d55ea0b516296f94cfed5311ee9ac4c0898618
Lou <lourdesgabriela423@gmail.com>
2026-09-01 10:57:25 -0300

    Subí los archivos del ejercicio 4
    Archivos .java Rectangulo y CreaFigura.
    Aun no estan 100% falta el javadoc y modificar la salida en pantalla

---
+++ tp3/CreaFigura.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Formatter;
+/**
+ * 
+ */
+public class CreaFigura {
+    public static void main (String args []){
+        
+        //intancia de objeto Punto
+        Punto2 punto1 = new Punto2(7.4, 4.5);
+
+        //nueva intancia de un objeto Rectangulo - 1
+        Random aleatorio = new Random();
+        double ancho = aleatorio.nextDouble() * 100.0; 
+        double alto = aleatorio.nextDouble() * 100.0;
+        
+        Rectangulo rectangulo1 = new Rectangulo(ancho, alto);//origen(0,0) 
+        rectangulo1.caracteristicas();
+        
+        //prueba 1
+        rectangulo1.desplazar(40.0, -20.0);
+        rectangulo1.caracteristicas();
+
+        //segunda instancia Rectangulo
+        double ancho2 = aleatorio.nextDouble() * 100.0; 
+        double alto2 = aleatorio.nextDouble() * 100.0;
+        Rectangulo rectangulo2 = new Rectangulo(punto1, ancho2, alto2);
+        
+        rectangulo2.caracteristicas();
+        
+        //mostrar el mayor rectangulo entre los dos
+        rectangulo1.elMayor(rectangulo2).caracteristicas();
+        
+        System.out.printf("\nLa distancia entre los dos rectangulos es: %.2f", rectangulo1.distanciaA(rectangulo2));
+
+        System.out.printf(""+rectangulo1.getAlto());
+
+    }
+}
---
+++ tp3/Rectangulo.java
@@ -0,0 +1,108 @@
+import java.util.Formatter;
+/**
+ * 
+ * Rectangulo
+ * 
+ * @author Lourdes Gabriela Caceres
+ * @version 1.0 27/08/2026
+ */
+public class Rectangulo {
+    
+    //variables de intancia
+    private Punto2 origen;
+    private double ancho;
+    private double alto;
+
+    /**
+     * 1er CONSTRUCTOR de la clase Rectangulo: para el caso en que el
+     * origen se encuentra en el punto (0,0), y solo recibe las dimensiones (ancho-alto)
+     * 
+     * @param p_ancho
+     * @param p_alto
+     */
+    public Rectangulo(double p_ancho, double p_alto){
+        
+        setOrigen(new Punto2(0,0));
+        setAncho(p_ancho);
+        setAlto(p_alto);
+    }
+
+    /**
+     * 2do CONSTRUCTOR de la clase Rectangulo: para el caso ...
+     * 
+     * @param p_origen
+     * @param p_ancho
+     * @param p_alto
+     */
+    public Rectangulo(Punto2 p_origen, double p_ancho, double p_alto){
+        setOrigen(p_origen);
+        setAncho(p_ancho);
+        setAlto(p_alto);
+    }
+
+    //Observadores (GETTERS)
+    public Punto2 getOrigen(){
+        return this.origen;
+    }
+    public double getAncho(){
+        return this.ancho;
+    }
+    public double getAlto(){
+        return this.alto;
+    }
+
+    //Mutadores (SETTERS)
+    private void setOrigen(Punto2 p_origen){
+        this.origen = p_origen;
+    } 
+    private void setAncho(double p_ancho){
+        this.ancho = p_ancho;
+    }
+    private void setAlto(double p_alto){
+        this.alto = p_alto;
+    }
+
+    /**
+     * 
+     */
+    public void desplazar(double p_dx, double p_dy){
+        this.getOrigen().desplazar(p_dx, p_dy);
+        System.out.print("\n\t=== punto desplazado correctamente");
+    }
+
+    /**
+     * Imprime en pantalla las caracteristicas del rectangulo:
+     * origen, alto y ancho. Asi como su superficie y el perímetro
+     */
+    public void caracteristicas(){
+        System.out.print("\n********** Rectangulo **********");
+        System.out.printf("\nOrigen: %s - Alto: %.2f - Ancho: %.2f", this.getOrigen().coordenadas(), this.getAlto(), this.getAncho());
+        System.out.printf("\nSuperficie: %.2f - Perimetro: %.2f\n", this.superficie(), this.perimetro());
+    }
+
+    public double perimetro(){
+        //perimetro = 2*(l1*l2)
+        return 2*(this.getAncho() + this.getAlto());
+    }
+    public double superficie(){
+        //area = base * altura
+        return this.getAncho() * this.getAlto();
+    }
+
+    public double distanciaA(Rectangulo otroRectangulo){
+        double distancia = this.getOrigen().distanciaA(otroRectangulo.getOrigen());
+        return distancia;
+    }
+
+    public Rectangulo elMayor(Rectangulo otroRectangulo){
+
+        if(this.superficie() == otroRectangulo.superficie()){
+            System.out.print("tienen la misma superficie");
+            return this;
+        }else if(this.superficie() > otroRectangulo.superficie()){
+            return this;
+        }
+        return otroRectangulo;
+    }
+
+}
